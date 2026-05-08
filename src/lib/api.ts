cb95b79b9e1e98cbddc21b7c279c9e0b7fa84b9c
KauaFMarques <kauamarques52@outlook.com>
2026-05-08 16:04:07 -0300

    tentando criar conexão com o backend no render

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://yp-game-backend2.onrender.com/";
 
 // ─── Tipos ────────────────────────────────────────────────────────────────────
 
